f2079d9b3ed7d954b0699d64168d63944a34f389
RichardENeumann <RichardNeumann@googlemail.com>
2024-05-31 14:40:27 +0200

    Add project Asm_ALU_SUB

---
+++ Asm_ALU_SUB/main.asm
@@ -0,0 +1,48 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;----------------
+; Main loop here
+;----------------
+main:
+			MOV.B	#0FFh, R4
+			MOV.B	#01h, R5
+			SUB.B	R5, R4
+
+			MOV.B	#01h, R6
+			MOV.B	#0FFh, R7
+			SUB.B	R7, R6
+
+			JMP		main
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
